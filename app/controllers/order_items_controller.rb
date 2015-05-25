d8aa290ed125bbd4b75127bda991fbd14077a9df
Roman Pavlov <pavlov.roman.top@gmail.com>
2015-05-25 12:05:58 +0200

    First version

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,6 +2,7 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
+    
     @order.save
     session[:order_id] = @order.id
   end
@@ -19,6 +20,11 @@ class OrderItemsController < ApplicationController
     @order_item.destroy
     @order_items = @order.order_items
   end
+
+  def incart
+    @order.order_items.find(params[:id]).nil?
+  end
+
 private
   def order_item_params
     params.require(:order_item).permit(:quantity, :product_id)
